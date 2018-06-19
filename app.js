0e3d95fc4e4c114eb626ddc12af6ceccf4624a37
Deubledee <dvsdias01@gmail.com>
2018-06-20 00:35:17 +0100

    app fix

--- app.js
+++ app.js
@@ -1,26 +1,11 @@
 const express = require('express');
 const expressSession = require("express-session")
-//var RedisStore = require('connect-redis')(expressSession);
-//const redis = require('redis');
-//const RedisServer = require('redis-server');
-//var commands = require('redis-commands');
 const path = require('path');
-const favicon = require('serve-favicon');
-const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const index = require('./routes/index');
-const resend = require('./routes/resend');
 const app = express();
 const responseTime = require('response-time')
-var obj = {
-  everything: [],
-  topHeadLines: [],
-  date: function () {
-    let t = new date()
-    return t
-  }
-}
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -30,10 +15,6 @@ app.use(expressSession({
   resave: true
 }));
 app.use(responseTime());
-/*app.use('/publicbower', express.static(path.join(__dirname, 'build/modern/public/bower_components/webcomponentsjs')));
-app.use('/public', express.static(path.join(__dirname, 'build/modern/public')));
-app.use('/src', express.static(path.join(__dirname, 'build/modern/src')));*/
-
 app.use('/publicbower', express.static(path.join(__dirname, 'public/bower_components/webcomponentsjs')));
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use('/socket', express.static(path.join(__dirname, 'node_modules/socket.io-client/dist')));
@@ -46,8 +27,3 @@ app.use('/removeelem/*', index);
 app.use('/profile/*', index);
 app.post('/audio/', resend);
 module.exports = app;
-/* upload.single('file'), function (req, res, next) {
-  // req.file is the `avatar` file
-  // req.body will hold the text fields, if there were any
-  console.log(req.body)
-}*/
