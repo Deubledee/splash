c7f791529fb9356442babb94308ce43b1ee30e30
Deubledee <dvsdias01@gmail.com>
2018-06-20 22:41:56 +0100

    app seesion removal

--- app.js
+++ app.js
@@ -10,10 +10,6 @@ const responseTime = require('response-time')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(expressSession({
-  secret: 'malaguenhaaaaa', saveUninitialized: true,
-  resave: true
-}));
 app.use(responseTime());
 app.use('/publicbower', express.static(path.join(__dirname, 'public/bower_components/webcomponentsjs')));
 app.use('/public', express.static(path.join(__dirname, 'public')));
